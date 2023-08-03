bd8713ab27f52eb36eeb3502cdf8dcafddef4fbf
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-08-03 14:59:27 +0100

    update milestone project

--- 08-Milestone Project - 2/milestone_project2.py
+++ 08-Milestone Project - 2/milestone_project2.py
@@ -4,13 +4,40 @@
 import random
 from collections import Counter
 
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
-          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 1}
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = (
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+)
+values = {
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+    "Six": 6,
+    "Seven": 7,
+    "Eight": 8,
+    "Nine": 9,
+    "Ten": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 1,
+}
+
 
 class Card:
-
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
@@ -18,16 +45,15 @@ class Card:
 
     def __str__(self):
         return f"{self.rank} of {self.suit}"
-    
+
     def __eq__(self, other):
         return self.value == other.value
-    
+
     def __lt__(self, other):
         return self.value < other.value
-    
+
 
 class Deck:
-
     def __init__(self):
         self.all_cards = []
         for suit in suits:
@@ -35,7 +61,7 @@ class Deck:
                 self.all_cards.append(Card(suit, rank))
 
     def __str__(self):
-        return '\n'.join(str(card) for card in self.all_cards)
+        return "\n".join(str(card) for card in self.all_cards)
 
     def __iter__(self):
         return iter(self.all_cards)
@@ -48,26 +74,24 @@ class Deck:
 
     def deal_one(self):
         return self.all_cards.pop()
-    
+
 
 class Player:
-
-    def __init__(self, name = 'One', money = 0):
+    def __init__(self, name="One", money=0):
         self.name = name
         self.all_cards = []
         self.value = 0
         self.money = money
-        
 
     def __str__(self):
         return f"Player {self.name} has {len(self.all_cards)} cards."
-    
+
     def __len__(self):
         return len(self.all_cards)
-    
+
     def hand(self):
-        return ', '.join(str(card) for card in self.all_cards)
-    
+        return ", ".join(str(card) for card in self.all_cards)
+
     def add_cards(self, new_cards):
         if type(new_cards) == type([]):
             self.all_cards.extend(new_cards)
@@ -86,11 +110,11 @@ class Player:
 
     def lose_money(self, amount):
         self.money -= amount
-        
+
     def initial_money_pot(self):
         while True:
             try:
-                self.add_money(int(input('How much money do you have for the game? ')))
+                self.add_money(int(input("How much money do you have for the game? ")))
             except TypeError:
                 print("Please input a number")
                 continue
@@ -100,7 +124,7 @@ class Player:
     def place_bet(self):
         while True:
             try:
-                self.bet = input('Place a bet: ')
+                self.bet = input("Place a bet: ")
                 if self.bet.isdigit() == False:
                     raise ValueError("Please input a number for your bet")
                 else:
@@ -115,40 +139,40 @@ class Player:
     def has_ace(self):
         ranks = [card.rank for card in self.all_cards]
         counts = Counter(ranks)
-        aces = counts['Ace']
+        aces = counts["Ace"]
         if aces > 0:
             self.value_with_aces = self.value
             while self.value_with_aces <= 11:
                 for _ in range(aces):
-                    self.value_with_aces += 10 
+                    self.value_with_aces += 10
         return aces
 
     def choose_aces(self):
-        player_choice = input("Would you like to choose the value with aces eleven? (yes/no) ")
-        if player_choice == 'yes':
+        player_choice = input(
+            "Would you like to choose the value with aces eleven? (yes/no) "
+        )
+        if player_choice == "yes":
             player.value = player.value_with_aces
-        
+
 
 class Dealer(Player):
-
-    def __init__(self, name = 'Dealer'):
+    def __init__(self, name="Dealer"):
         super().__init__(name)
-    
+
     def __str__(self):
         return f"{self.name} has {len(self.all_cards)} cards."
 
 
-
-def player_turn(player, deck, round_end = False):
-    """ function to run the player's turn in a Blackjack game """
+def player_turn(player, deck, round_end=False):
+    """function to run the player's turn in a Blackjack game"""
     player_turn = True
-    print(f'Player {player.name} to play')
+    print(f"Player {player.name} to play")
     while player_turn:
-        player_choice = input('Would you like to hit or stand? ')
-        if player_choice.lower() == 'hit':
+        player_choice = input("Would you like to hit or stand? ")
+        if player_choice.lower() == "hit":
             new_card = deck.deal_one()
             player.add_cards(new_card)
-            print(f'Your hand is {player.hand()}')
+            print(f"Your hand is {player.hand()}")
             if player.value > 21:
                 print(f"Bust! Player {player.name} loses")
                 player.lose_money(player.bet)
@@ -156,31 +180,34 @@ def player_turn(player, deck, round_end = False):
                 player_turn = False
                 break
             elif player.has_ace() > 0:
-                    if player.value_with_aces == player.value:
-                         print(f'Your current total is {player.value}')
-                    else:
-                        print(f'Your current total is {player.value}, or {player.value_with_aces} with aces as eleven')
-                        player.choose_aces()
+                if player.value_with_aces == player.value:
+                    print(f"Your current total is {player.value}")
+                else:
+                    print(
+                        f"Your current total is {player.value}, or {player.value_with_aces} with aces as eleven"
+                    )
+                    player.choose_aces()
             else:
-                print(f'Your current total is {player.value}')      
-        elif player_choice.lower() == 'stand':
+                print(f"Your current total is {player.value}")
+        elif player_choice.lower() == "stand":
             player_turn = False
             break
         else:
-            print('Invalid choice, please choose again.')
+            print("Invalid choice, please choose again.")
     return round_end, player
 
-def dealer_turn(dealer, player, deck, round_end = False):
-    """ function to run the automated dealer's turn in a Blackjack game """
+
+def dealer_turn(dealer, player, deck, round_end=False):
+    """function to run the automated dealer's turn in a Blackjack game"""
     dealer_turn = True
-    print(f'Dealer to play')
+    print(f"Dealer to play")
     while dealer_turn:
         if dealer.value < player.value:
             new_card = deck.deal_one()
             dealer.add_cards(new_card)
             if dealer.has_ace() > 0:
                 if dealer.value_with_aces > player.value and dealer.value <= 21:
-                    print(f'Dealer has {dealer.value}, dealer wins')
+                    print(f"Dealer has {dealer.value}, dealer wins")
                     player.lose_money(player.bet)
                     round_end = True
                     dealer_turn = False
@@ -188,72 +215,69 @@ def dealer_turn(dealer, player, deck, round_end = False):
                 break
         elif dealer.value > 21:
             print(f"Bust! Player {player.name} wins")
-            player.add_money(2*player.bet)
+            player.add_money(2 * player.bet)
             round_end = True
             dealer_turn = False
             break
         elif dealer.value > player.value and dealer.value > 21:
             print(f"Bust! Player {player.name} wins")
-            player.add_money(2*player.bet)
+            player.add_money(2 * player.bet)
             round_end = True
             dealer_turn = False
             break
         elif dealer.value > player.value and dealer.value <= 21:
-            print(f'Dealer has {dealer.value}, dealer wins')
+            print(f"Dealer has {dealer.value}, dealer wins")
             player.lose_money(player.bet)
             round_end = True
             dealer_turn = False
-            break    
+            break
     return round_end, dealer, player
 
 
-
 if __name__ == "__main__":
-
     # define dealer and player
     dealer = Dealer()
-    player_name = input('Choose a name: ')
+    player_name = input("Choose a name: ")
     player = Player(player_name)
 
     # get player total money pot
     player.initial_money_pot()
-    
-    round_number = 0 
+
+    round_number = 0
     game_on = True
 
     while game_on:
         round_number += 1
         round_end = False
 
-        
         new_deck = Deck()
         new_deck.shuffle()
 
         print(f"\nRound {round_number}\n")
-            
+
         # deal initial cards
         for _ in range(2):
-                print('Dealing cards...')
-                player.add_cards(new_deck.deal_one())
-                dealer.add_cards(new_deck.deal_one())
-            
-        print('Initial hand dealt')
-        print(f'Your hand is {player.hand()}')
+            print("Dealing cards...")
+            player.add_cards(new_deck.deal_one())
+            dealer.add_cards(new_deck.deal_one())
+
+        print("Initial hand dealt")
+        print(f"Your hand is {player.hand()}")
 
         # inform player of current hand value, including any aces present
         if player.has_ace() > 0:
             print(f"You have an Ace!")
-            print(f'Your current total is {player.value}, or {player.value_with_aces} with aces as eleven')
+            print(
+                f"Your current total is {player.value}, or {player.value_with_aces} with aces as eleven"
+            )
             player.choose_aces()
         else:
-            print(f'Your current total is {player.value}')
-
+            print(f"Your current total is {player.value}")
 
         # place bet
         player.place_bet()
 
         while round_end == False:
-
             # player turn
             round_end, player = player_turn(player, new_deck)
 
@@ -262,7 +286,6 @@ if __name__ == "__main__":
 
             # dealer turn
             round_end, dealer, player = dealer_turn(dealer, player, new_deck)
-        
 
         player.remove_hand()
         dealer.remove_hand()
@@ -272,11 +295,8 @@ if __name__ == "__main__":
             game_on = False
         else:
             print(f"Your current bankroll is {player.money}")
-            game = input('Do you want to continue? (yes/no) ')
-            if game.lower() in ['y', 'yes']:
+            game = input("Do you want to continue? (yes/no) ")
+            if game.lower() in ["y", "yes"]:
                 pass
             else:
                 game_on = False
-
-                
-
--- 08-Milestone Project - 2/tests/test_project.py
+++ 08-Milestone Project - 2/tests/test_project.py
@@ -1,15 +1,18 @@
 import milestone_project2
 
+
 def test_card_value():
-    assert milestone_project2.Card("Hearts","Two").value == 2
-    
+    assert milestone_project2.Card("Hearts", "Two").value == 2
+
+
 def test_picture_card_value():
-    assert milestone_project2.Card("Hearts","King").value == 10
+    assert milestone_project2.Card("Hearts", "King").value == 10
+
 
 def test_player_add_one_card():
     player = milestone_project2.Player()
-    player.add_cards(milestone_project2.Card("Hearts","Two"))
-    assert str(player.all_cards[0]) == 'Two of Hearts'
+    player.add_cards(milestone_project2.Card("Hearts", "Two"))
+    assert str(player.all_cards[0]) == "Two of Hearts"
 
 
 def test_player_win():
@@ -17,15 +20,20 @@ def test_player_win():
     player_win = False
     player_value = 19
     dealer = milestone_project2.Dealer()
-    dealer.add_cards([milestone_project2.Card("Hearts","Two"),milestone_project2.Card("Hearts","King")])
+    dealer.add_cards(
+        [
+            milestone_project2.Card("Hearts", "Two"),
+            milestone_project2.Card("Hearts", "King"),
+        ]
+    )
     dealer_turn = True
     while dealer_turn:
         if dealer.value < player_value:
-            new_card = milestone_project2.Card("Clubs","Queen")
+            new_card = milestone_project2.Card("Clubs", "Queen")
             dealer.add_cards(new_card)
             if dealer.has_ace() > 0:
                 if dealer.value_with_aces > player_value:
-                    dealer_win = 'aces'
+                    dealer_win = "aces"
                     break
                 break
         elif dealer.value > 21:
@@ -39,19 +47,25 @@ def test_player_win():
             break
     assert player_win == True
 
+
 def test_dealer_win():
     dealer_win = False
     player_value = 19
     dealer = milestone_project2.Dealer()
-    dealer.add_cards([milestone_project2.Card("Hearts","Two"),milestone_project2.Card("Hearts","King")])
+    dealer.add_cards(
+        [
+            milestone_project2.Card("Hearts", "Two"),
+            milestone_project2.Card("Hearts", "King"),
+        ]
+    )
     dealer_turn = True
     while dealer_turn:
         if dealer.value < player_value:
-            new_card = milestone_project2.Card("Clubs","Eight")
+            new_card = milestone_project2.Card("Clubs", "Eight")
             dealer.add_cards(new_card)
             if dealer.has_ace() > 0:
                 if dealer.value_with_aces > player_value:
-                    dealer_win = 'aces'
+                    dealer_win = "aces"
                     break
                 break
         elif dealer.value > 21:
@@ -65,19 +79,25 @@ def test_dealer_win():
             break
     assert dealer_win == True
 
+
 def test_dealer_win_ace():
     dealer_win = False
     player_value = 19
     dealer = milestone_project2.Dealer()
-    dealer.add_cards([milestone_project2.Card("Hearts","Two"),milestone_project2.Card("Hearts","Eight")])
+    dealer.add_cards(
+        [
+            milestone_project2.Card("Hearts", "Two"),
+            milestone_project2.Card("Hearts", "Eight"),
+        ]
+    )
     dealer_turn = True
     while dealer_turn:
         if dealer.value < player_value:
-            new_card = milestone_project2.Card("Clubs","Ace")
+            new_card = milestone_project2.Card("Clubs", "Ace")
             dealer.add_cards(new_card)
             if dealer.has_ace() > 0:
                 if dealer.value_with_aces > player_value:
-                    dealer_win = 'aces'
+                    dealer_win = "aces"
                     break
                 break
         elif dealer.value > 21:
@@ -89,4 +109,4 @@ def test_dealer_win_ace():
         elif dealer.value > player_value:
             dealer_win = True
             break
-    assert dealer_win == 'aces'
+    assert dealer_win == "aces"
