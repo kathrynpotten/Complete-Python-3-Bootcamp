0184472d12057ffef3090ec4def4a68ed1599c53
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-08-07 15:23:44 +0100

    advanced module puzzle solution

---
+++ 12-Advanced Python Modules/08-Advanced-Python-Module-Exercise/advanced_module_puzzle.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+import re
+
+
+# shutil.unpack_archive("unzip_me_for_instructions.zip", "instructions", "zip")
+
+
+path = "C:/Users/kathr/OneDrive/dev/Complete-Python-3-Bootcamp/12-Advanced Python Modules/08-Advanced-Python-Module-Exercise/instructions/extracted_content"
+for folder, sub_folders, files in os.walk(path):
+    for file in files:
+        with open(folder + "/" + file, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+        for line in lines:
+            pattern = "\d{3}-\d{3}-\d{4}"
+            phone_number = re.findall(pattern, line)
+            if phone_number != []:
+                print(f"The phone number is in {folder}")
+                print(phone_number[0])
