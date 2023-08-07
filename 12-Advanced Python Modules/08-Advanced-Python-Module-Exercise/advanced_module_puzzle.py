447eb2760f643623d33ad58033718fdef16bd329
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-08-07 15:30:01 +0100

    advanced modules puzzle solution

--- 12-Advanced Python Modules/08-Advanced-Python-Module-Exercise/advanced_module_puzzle.py
+++ 12-Advanced Python Modules/08-Advanced-Python-Module-Exercise/advanced_module_puzzle.py
@@ -10,10 +10,9 @@ path = "C:/Users/kathr/OneDrive/dev/Complete-Python-3-Bootcamp/12-Advanced Pytho
 for folder, sub_folders, files in os.walk(path):
     for file in files:
         with open(folder + "/" + file, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-        for line in lines:
-            pattern = r"\d{3}-\d{3}-\d{4}"
-            phone_number = re.findall(pattern, line)
-            if phone_number != []:
-                print(f"The phone number is in {folder}")
-                print(phone_number[0])
+            text = f.read()
+        pattern = r"\d{3}-\d{3}-\d{4}"
+        phone_number = re.findall(pattern, text)
+        if phone_number != []:
+            print(f"The phone number is in {folder}")
+            print(phone_number[0])
